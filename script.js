198aaadcea464bdf0b0e1a6b90d2b2a03e8c14b4
hyosetsu <131899359+hyosetsu@users.noreply.github.com>
2024-03-30 15:07:43 +0900

    少し訂正

--- script.js
+++ script.js
@@ -38,7 +38,54 @@ async function translateText(text) {
     )}&target_lang=JA`
   );
   const data = await response.json();
-  document.getElementById("translatedText").textContent =
-    data.translations[0].text;
+  const translatedTextContainer = document.getElementById(
+    "translatedTextContainer"
+  );
+  translatedTextContainer.innerHTML = `<div class="translatedTextBox">${data.translations[0].text}</div>`;
   document.getElementById("translation").style.display = "block";
 }
+
+// let extractedText = "";
+
+// async function uploadImage() {
+//   const fileInput = document.getElementById("fileInput");
+//   const file = fileInput.files[0];
+//   if (!file) {
+//     alert("ファイルを選択してください");
+//     return;
+//   }
+
+//   const uploadedImage = document.getElementById("uploadedImage");
+//   uploadedImage.src = URL.createObjectURL(file);
+//   document.getElementById("imageContainer").style.display = "block";
+
+//   // Tesseract.jsを初期化し、画像からテキストを抽出する
+//   const {
+//     data: { text },
+//   } = await Tesseract.recognize(file);
+
+//   extractedText = text;
+//   const extractedTextElement = document.getElementById("extractedText");
+//   extractedTextElement.textContent = extractedText;
+//   extractedTextElement.focus();
+//   document.getElementById("textContainer").style.display = "block";
+// }
+
+// function correctText() {
+//   const correctedText = document.getElementById("extractedText").value;
+//   extractedText = correctedText;
+//   translateText(correctedText);
+// }
+
+// async function translateText(text) {
+//   const apiKey = "3d7e3da4-defa-46ed-9592-4a909a08c449:fx";
+//   const response = await fetch(
+//     `https://api-free.deepl.com/v2/translate?auth_key=${apiKey}&text=${encodeURIComponent(
+//       text
+//     )}&target_lang=JA`
+//   );
+//   const data = await response.json();
+//   document.getElementById("translatedText").textContent =
+//     data.translations[0].text;
+//   document.getElementById("translation").style.display = "block";
+// }
